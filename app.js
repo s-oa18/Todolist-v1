e1d97086157bb2ead91dd864c786b22fdc707de4
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2022-09-11 23:37:29 +0000

    installed and required express and body-parser

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("hello");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on pport 3000");
+});
