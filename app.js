4cdb835c0793b9a1d544acb4d147c1577d430718
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2022-09-13 20:19:19 +0000

    adding functionalities-app.post

--- app.js
+++ app.js
@@ -5,46 +5,20 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
+  var options = { weekday: "long", day: "numeric", month: "long" };
 
-    case 1:
-      day = "Monday";
-      break;
-
-    case 2:
-      day = "Tuesday";
-      break;
-
-    case 3:
-      day = "Wednesday";
-      break;
-
-    case 4:
-      day = "Thursday";
-      break;
-
-    case 5:
-      day = "Friday";
-      break;
-
-    case 6:
-      day = "Saturday";
-      break;
-
-    default:
-      console.log("Error: current day is equal to: " + currentDay);
-      break;
-  }
+  var day = today.toLocaleDateString("en-US", options);
 
   res.render("list", { kindOfDay: day });
+});
+
+app.post("/", function (req, res) {
+  console.log(req.body.newItem);
 });
 
 app.listen(3000, function () {
