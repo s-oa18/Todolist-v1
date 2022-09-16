e12c17ab1ebe792253c4ef2e25f6f4fde4b99cd6
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2022-09-16 23:22:07 +0000

    template and styles.css updates

--- app.js
+++ app.js
@@ -3,22 +3,29 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = ["Buy food", "Cook food", "Eat food"];
+
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  var today = new Date();
+  let today = new Date();
 
-  var options = { weekday: "long", day: "numeric", month: "long" };
+  let options = { weekday: "long", day: "numeric", month: "long" };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day });
+  res.render("list", { kindOfDay: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-  console.log(req.body.newItem);
+  let item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
